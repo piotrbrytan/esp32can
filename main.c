b4ac1e49cec29824c10c25d8df6a85c8394b8521
Piotrek <brytan.piotr@gmail.com>
2019-04-10 18:27:01 +0200

    miganie dioda taskami FreeRTOS

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
 #include "driver/gpio.h"
-#include "sdkconfig.h"
 
-#define BLINK_GPIO 2 //wbudowana dioda
+#define BLINK_GPIO 2
 
-void app_main(){
+void blinky(void *pvParameter) {
 
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT); //ustawienie pinu na wyjscie
+	gpio_pad_select_gpio(BLINK_GPIO);
+	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+	while (1) {
+		gpio_set_level(BLINK_GPIO, 0);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+		gpio_set_level(BLINK_GPIO, 1);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+	}
+}
 
-    while(1) {                                  //nieskonczona petla
-	    printf("Turning off the LED\n");        //przeslanie wiadomosci 
-        gpio_set_level(BLINK_GPIO, 0);          //ustawienie stanu nieskiego
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  //opoznienie 1000ms
-	    printf("Turning on the LED\n");         //przeslanie wiadomosci
-        gpio_set_level(BLINK_GPIO, 1);          //ustawienie stanu wysokiego
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  //opoznienie 1000ms
-    }
+void app_main() {
+	xTaskCreate(&blinky, "blinky", 512, NULL, 1, NULL);
 }
