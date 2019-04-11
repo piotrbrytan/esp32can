e8bcdd72e80fee66a6a619426701abc1c19d26f1
Piotrek <brytan.piotr@gmail.com>
2019-04-11 18:09:02 +0200

    dodanie obslugi wifi

--- main.c
+++ main.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+
 #include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event_loop.h"
+
+#include "nvs_flash.h"
+
 #include "driver/gpio.h"
 
-#define BLINK_GPIO 2
+#define BLINK_GPIO 2 //wbudowana dioda
+#define CONFIG_WIFI_SSID "" //ssid
+#define CONFIG_WIFI_PASSWORD "" //haslo do wifi
 
+//prototypy funkcji, taksow
+static void wifi_init(void);
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event);
+void blinky(void *pvParameter);
+
+//delkaracja zmiennych
+static const char *TAG = "CAN_WiFi";
+static EventGroupHandle_t wifi_event_group;
+const static int CONNECTED_BIT = BIT0;
+
+//funkcja main
+void app_main() {
+	xTaskCreate(&blinky, "blinky", 512, NULL, 1, NULL);
+
+    nvs_flash_init();
+    wifi_init();
+}
+
+//funkcja migajaca dioda
 void blinky(void *pvParameter) {
 
 	gpio_pad_select_gpio(BLINK_GPIO);
@@ -18,6 +47,46 @@ void blinky(void *pvParameter) {
 	}
 }
 
-void app_main() {
-	xTaskCreate(&blinky, "blinky", 512, NULL, 1, NULL);
+//polaczenie z wifi
+static void wifi_init(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_WIFI_SSID,
+            .password = CONFIG_WIFI_PASSWORD,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_LOGI(TAG, "start the WIFI SSID:[%s]", CONFIG_WIFI_SSID);
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "Waiting for wifi");
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+}
+
+//event handler do wifi
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
+{
+    switch (event->event_id) {
+        case SYSTEM_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            esp_wifi_connect();
+            xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
 }
